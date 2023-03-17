12e6394eac87c1a6ffa28debf45236f9176c4b27
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:22:29 -0700

    Create jquerify v1.js

---
+++ other/jquerify v1.js
@@ -0,0 +1,1 @@
+javascript:var s=document.createElement('script');s.setAttribute('src', 'http://jquery.com/src/jquery-latest.js');document.getElementsByTagName('body')[0].appendChild(s);alert('thank you for using jquery!');void(s);
