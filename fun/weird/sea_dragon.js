7ee737af668475b43de9e329809eefaaf2503c5b
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:19:27 -0700

    Create sea_dragon.js

---
+++ fun/weird/sea_dragon.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bfunction callback()%7Bwindow.SeaDragon.add()%7Dvar s%3Ddocument.createElement("script")%3Bs.src%3D"https%3A%2F%2Fmrcoles.com%2Fmedia%2Fjs%2Fseadragon.js"%3Bif(s.addEventListener)%7Bs.addEventListener("load"%2Ccallback%2Cfalse)%7Delse if(s.readyState)%7Bs.onreadystatechange%3Dcallback%7Ddocument.body.appendChild(s)%3B%7D)()
