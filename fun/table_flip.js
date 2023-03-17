53aeb315f622b5a8135bd43fd8243ff1be549931
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:17:20 -0700

    Create table_flip.js

---
+++ fun/table_flip.js
@@ -0,0 +1,3 @@
+javascript:!function(){let o=["(╯\xb0□\xb0）╯︵ ┻━┻","(┛◉Д◉)┛彡┻━┻","(ﾉ≧∇≦)ﾉ ﾐ ┸━┸","(ノಠ益ಠ)ノ彡┻━┻","(╯ರ ~ ರ）╯︵ ┻━┻","(┛ಸ_ಸ)┛彡┻━┻","(ﾉ\xb4･ω･)ﾉ ﾐ ┸━┸","(ノಥ,_｣ಥ)ノ彡┻━┻","(┛✧Д✧))┛彡┻━┻",],e;do e=o[Math.floor(Math.random()*o.length)];while(!window.confirm(e));setTimeout(()=>navigator.clipboard.writeText(e),100)}();
+
+// press cancel to choose another one, press ok to copy the text.
