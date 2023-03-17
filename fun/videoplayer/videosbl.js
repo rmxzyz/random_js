5a364155f0c2e174ed4e53e4ff5543a33e6e40de
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 14:55:04 -0700

    Create videosbl.js

---
+++ fun/videoplayer/videosbl.js
@@ -0,0 +1,19 @@
+//Rickroll
+
+javascript:void function(){var e=document.createElement("video");e.src="https://ia601602.us.archive.org/11/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4",e.style="position:fixed;left:450px;top:100px;width:640px;height:480px",e.autoplay=!0,document.body.appendChild(e)}();
+
+//Amongus theme song remix
+
+javascript:void function(){var e=document.createElement("video");e.src="https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/AMONG US Theme Song (Moondai EDM Remix).mp4",e.style="position:fixed;left:450px;top:100px;width:640px;height:480px",e.autoplay=!0,document.body.appendChild(e)}();
+
+// Never gonna meow you up
+
+javascript:void function(){var e=document.createElement("video");e.src="https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/Never gonna Meow you up.mp4",e.style="position:fixed;left:450px;top:100px;width:640px;height:480px",e.autoplay=!0,document.body.appendChild(e)}();
+
+//Cat purring for 20 minutes  ( made tiny )  * i had to find a small one because the other ones were over 25 mb.
+
+// do later : filesize over 20 mb for jsdeliver
+
+https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/20%20Minutes%20Of%20_%20Cat%20Purring%20_%20Relaxing%20Audio.mp4
+
+
