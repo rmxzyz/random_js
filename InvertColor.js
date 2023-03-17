3877163b37ed52d47ccfca082416d264d8a7d34e
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:08:55 -0700

    Create InvertColor.js

---
+++ InvertColor.js
@@ -0,0 +1,2 @@
+javascript:krazete:var inverthtml;void 0===inverthtml?((inverthtml=document.createElement("style")).innerHTML="html{background:black;filter:invert(100%25)}",document.body.appendChild(inverthtml)):(inverthtml.remove(),inverthtml=void 0);
+
