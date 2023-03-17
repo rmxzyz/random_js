f72620da3dbe55fec2498aa3ed32daedc259a17b
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:21:19 -0700

    Create tornado.js

---
+++ fun/tornado.js
@@ -0,0 +1,3 @@
+javascript:(function(){document.documentElement.style.transitionDuration="60s";document.documentElement.style.transitionTimingFunction="ease-in";document.documentElement.style.transform="rotate(360000deg)";}());
+
+//make webpage spin
