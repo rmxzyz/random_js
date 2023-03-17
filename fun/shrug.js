9f56a911c95afc1dd9dff6a6e03840b275345e28
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:21:57 -0700

    Create shrug.js

---
+++ fun/shrug.js
@@ -0,0 +1,3 @@
+javascript: void(function() {  /* More bookmarklets at https://gist.github.com/iamandrewluca/61feacf07bc4f2f50e70f986c2e9b2d2 */  const all = [    "¯\\_(ツ)_/¯",    "¯\\(ツ)/¯",    "ʅ(ツ)ʃ",    "乁(ツ)ㄏ",    "乁(ツ)∫",    "ƪ(ツ)∫",    "¯\\_₍ッ₎_/¯",    "乁₍ッ₎ㄏ",    "ʅ₍ッ₎ʃ",    "¯\\_(シ)_/¯",    "¯\\_(ツ゚)_/¯",    "乁(ツ゚)ㄏ",    "¯\\_㋡_/¯",    "┐_㋡_┌",    "┐_(ツ)_┌━☆ﾟ.*･｡ﾟ",  ];  while(!window.confirm(all[Math.floor(Math.random() * all.length)])) {}}());
+
+//same for table flip but with shrug
