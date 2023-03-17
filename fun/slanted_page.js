1d178e3e3c49d0056fa6050f1a5cb5964a1daa81
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 13:49:18 -0700

    Create slanted_page.js

---
+++ fun/slanted_page.js
@@ -0,0 +1,8 @@
+//big
+
+javascript:Array.prototype.slice.call(  document.querySelectorAll(    'div,p,span,img,a,body')).map(function(tag){tag.style['transform'] = 'rotate(' + (    Math.floor(Math.random() * 2.2) - 1) + 'deg)';});
+
+//small
+
+
+
