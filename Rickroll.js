b58e12221f2a57bc7213e4ea53384194bbac829d
rmxzyz <128182562+rmxzyz@users.noreply.github.com>
2023-03-17 12:09:36 -0700

    Create Rickroll.js

---
+++ Rickroll.js
@@ -0,0 +1,8 @@
+//No Control:
+
+javascript:void function(){var e=document.createElement("video");e.src="https://ia601602.us.archive.org/11/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4",e.style="position:fixed;left:450px;top:100px;width:640px;height:480px",e.autoplay=!0,document.body.appendChild(e)}();
+
+//Control
+
+javascript:void function(){var e=document.createElement("video");e.src="https://ia601602.us.archive.org/11/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4",e.style="position:fixed;left:450px;top:100px;width:640px;height:480px",e.autoplay=!0,e.controls=!0,document.body.appendChild(e)}();
+
