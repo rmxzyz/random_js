d7baf6d193e7d9771e68e556bd91ebee86ea119f
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-18 09:29:19 -0700

    Create focus-order.js

---
+++ other/focus-order.js
@@ -0,0 +1,1 @@
+javascript:(function(){var element=document.createElement('script'); element.setAttribute('src','https://mraccess77.github.io/favlets/focusOrder.js'); document.body.appendChild(element);})();
