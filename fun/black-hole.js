4799ce519c69b71d026c12d9b22584aef910af7b
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-18 09:19:13 -0700

    Create black-hole.js

---
+++ fun/black-hole.js
@@ -0,0 +1,4 @@
+javascript:(function(){var x = document.getElementsByTagName("*"); for (i = 0; i < x.length; i++) {x[i].style.position = "absolute"; x[i].style.top = "50%"; x[i].style.left = "50%"; x[i].style.transition = "5s"; x[i].style.transform = "translate(-50%, -50%)"}}());
+
+
+// ill make a more realistic one later
