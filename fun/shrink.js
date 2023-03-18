8affbf6cee8f92aee8353d2f046440db5ee12c6d
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-18 09:20:33 -0700

    Create shrink.js

---
+++ fun/shrink.js
@@ -0,0 +1,2 @@
+javascript:(function(){document.documentElement.style.transitionDuration="10s";document.documentElement.style.transformOrigin = "center, top";document.documentElement.style.transitionTimingFunction="ease-in";document.documentElement.style.transform="scale(0.1)";}());
+
