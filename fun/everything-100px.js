c0655ad9f556876590b53e2000a1473285b8083f
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-18 09:21:52 -0700

    Create everything-100px.js

---
+++ fun/everything-100px.js
@@ -0,0 +1,2 @@
+javascript:(function(){var x = document.body.getElementsByTagName("*"); for (i = 0; i < x.length; i++) {x[i].style.transition = "10s"}; document.body.style.fontSize = "100px";}());
+
