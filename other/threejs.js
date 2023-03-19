9d0d05995d76020e312079996b5990d7a82d1882
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-18 17:22:15 -0700

    Create threejs.js

---
+++ other/threejs.js
@@ -0,0 +1,3 @@
+javascript:!function(){var e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js",e.onload=function(){console.log("Three.js loaded!")},document.head.appendChild(e)}();
+
+//Injects THREE.js into the webpage.
