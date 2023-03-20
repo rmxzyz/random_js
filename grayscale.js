55d6881bc28a15d730c9ee6a5ccd94b374aaff5a
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-20 11:38:12 -0700

    Create grayscale.js

---
+++ grayscale.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var element=document.createElement('script'); element.setAttribute('src','https://mraccess77.github.io/favlets/grayscale.js'); document.body.appendChild(element); } ) ();
