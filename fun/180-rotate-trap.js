e23fa177d5a3ceb37ddf499699724250351d71c8
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-20 08:15:28 -0700

    Create 180-rotate-trap.js

---
+++ fun/180-rotate-trap.js
@@ -0,0 +1,3 @@
+javascript:setTimeout(function(){document.onmousemove=document.onkeypress=function(){["","-ms-","-webkit-","-o-","-moz-"].map(function(o){document.body.style[o+"transition"]=o+"transform 3s",document.body.style[o+"transform"]="rotate(180deg)"})}},5e3);
+
+//when somebody moves their mouse, it waits 5 seconds and flips the page 180.
