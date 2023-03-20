d45738d0eef63a604bb091f11727a35e41adff9c
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-20 08:14:02 -0700

    Create flip-180.js

---
+++ fun/flip-180.js
@@ -0,0 +1,4 @@
+	['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){
+			document.body.style[prefix + 'transition'] = prefix + 'transform 3s';
+			document.body.style[prefix + 'transform'] = 'rotate(180deg)';
+		});
