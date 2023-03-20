4abcc03a868709434efb99b985c8116e75a54f1e
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-20 07:23:57 -0700

    Create ANDI.js

---
+++ other/ANDI.js
@@ -0,0 +1,5 @@
+//normal
+javascript:void((function(){andiScript=document.createElement('script');andiScript.setAttribute('src','https://www.ssa.gov/accessibility/andi/andi.js');document.body.appendChild(andiScript)})());
+
+//school
+javascript:void((function(){andiScript=document.createElement('script');andiScript.setAttribute('src','https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/andi.js');document.body.appendChild(andiScript)})());
