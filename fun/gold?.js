12089f7687be6860669ed8d3361f40a0a9eb6b71
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 14:07:33 -0700

    Create gold?.js

---
+++ fun/gold?.js
@@ -0,0 +1,4 @@
+javascript:(function(){document.body.style.backgroundColor='gold';var elements=document.getElementsByTagName('*');for(var i=0;i<elements.length;i++){elements[i].style.color='gold';elements[i].style.borderColor='gold';}})();
+
+
+//i'll make a realistic / shiny one later.
