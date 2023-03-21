356c124e9e923cad1de0876890e2b516687ed393
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 10:50:21 -0700

    Create removeImages.js

---
+++ other/removeImages.js
@@ -0,0 +1,1 @@
+javascript: (function() {function toArray(c) {var a, k;a = new Array;for (k = 0; k < c.length; ++k) a[k] = c[k];return a;}var images, img, altText;images = toArray(document.images);for (var i = 0; i < images.length; ++i) {img = images[i];altText = document.createTextNode(img.alt);img.parentNode.replaceChild(altText, img)}})();
