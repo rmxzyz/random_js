e81fe472c334db888ba3883c6d206e48b5f6c5ab
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 08:04:10 -0700

    Update gravity.js

--- gravity.js
+++ gravity.js
@@ -1,2 +1,8 @@
 javascript:var script = document.createElement("script"); script.src="https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/gravity.js"; document.body.appendChild(script);void(0);
 
+/* FIX : 
+Uncaught TypeError: els[i].className.split is not a function
+    at getElementsByClass (gravity.js:1804:37) <---
+    at init (gravity.js:1464:16)  ^^^^^^^^^^^^^
+    at gravity.js:1430:4
+*/
