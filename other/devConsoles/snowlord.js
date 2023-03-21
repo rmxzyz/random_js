f290167d8a046737387bd473d28e16b888b2c42a
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 11:26:24 -0700

    Create snowlord.js

---
+++ other/devConsoles/snowlord.js
@@ -0,0 +1,1 @@
+javascript:(function(){(function(){var x = document.createElement("script");x.src="https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";document.head.appendChild(x);})()})()
