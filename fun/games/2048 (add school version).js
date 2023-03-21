86d0a1c4066489699fc1017801d2265a1f6ada41
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 10:44:36 -0700

    Create 2048 (add school version).js

---
+++ fun/games/2048 (add school version).js
@@ -0,0 +1,6 @@
+//Regular
+
+javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://sheeptester.github.io/javascripts/2048.js';document.body.appendChild(s);})('script')
+
+
+//School
