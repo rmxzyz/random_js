549e945fcc5d6f010eebdace85ab0c849645a9d0
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 11:17:24 -0700

    Create flashes.js

---
+++ fun/flashes.js
@@ -0,0 +1,7 @@
+// Warning : These have flashing lights. If you are sensitive, I prefer not to use these.
+
+//black and green
+javascript: document.body.style.backgroundColor = "black"; baw = function() {var color = document.body.style.backgroundColor; if(color == "black") { document.body.style.backgroundColor = "rgb(100, 255, 100)";}else {if (color = "rbg(100, 255, 100)") {document.body.style.backgroundColor = "black"}else {alert("Failed")}}}; setInterval(baw, 20); void 0
+
+//black and red
+javascript: document.body.style.backgroundColor = "black"; baw = function() {var color = document.body.style.backgroundColor; if(color == "black") { document.body.style.backgroundColor = "red";}else {if (color = "red") {document.body.style.backgroundColor = "black"}else {alert("Failed")}}}; setInterval(baw, 20); void 0
