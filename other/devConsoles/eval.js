165a121ff1473f97603872263e648c5cc408ad2d
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 16:58:02 -0700

    Create eval.js

---
+++ other/devConsoles/eval.js
@@ -0,0 +1,3 @@
+javascript:var s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/eval.js';document.body.appendChild(s);void(0);
+
+//credits to sheeptester.github.io
