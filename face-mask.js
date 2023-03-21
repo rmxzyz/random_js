212fbe083076734bb5c8aa121dbad471c951908b
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 07:54:50 -0700

    Create face-mask.js

---
+++ face-mask.js
@@ -0,0 +1,3 @@
+javascript: const faces = ['OwO ', 'UwU', 'TwT', '>w<', '^w^']; let face = faces[Math.floor(Math.random()*faces.length)]; document.write('<body style=\'background-color: black;\'></body><title>' + face + '</title><p style=\'text-align: center; font-size: 500px; font-family: Arial; color: white;\'>' + face + '</p>');
+
+//edit the "const faces = []" for different items
