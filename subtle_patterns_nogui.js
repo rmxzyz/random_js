5357081d8fde510b620c00960fe0bed55dac97f0
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 13:45:27 -0700

    Create subtle_patterns_nogui.js

---
+++ subtle_patterns_nogui.js
@@ -0,0 +1,3 @@
+javascript:(function(){var newscript=document.createElement('script');newscript.type='text/javascript';newscript.async=true;newscript.src='https://bradjasper.com/subtle-patterns-bookmarklet/static/js/all.js?cb=' + Math.random();(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);})();
+
+//left arrow and right arrow to change patterns
