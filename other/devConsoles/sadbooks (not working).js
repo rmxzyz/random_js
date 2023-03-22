8445da1b3a8aebb1ebbe5103a1f6f874762169cb
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 17:55:48 -0700

    Create sadbooks (not working).js

---
+++ other/devConsoles/sadbooks (not working).js
@@ -0,0 +1,1 @@
+javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/sadbooks.js';document.body.appendChild(s);})('script')
