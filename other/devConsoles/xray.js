f77dbd7963f8bb2c95e41f7b70b7ea660125d840
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 07:49:29 -0700

    Create xray.js

---
+++ other/devConsoles/xray.js
@@ -0,0 +1,5 @@
+//Normal
+javascript:(function () {var script=document.createElement('script');script.src='https://x-ray-goggles.mouse.org/webxray.js';script.className='webxray';script.setAttribute('data-lang','en-US');script.setAttribute('data-baseuri','https://x-ray-goggles.mouse.org');document.body.appendChild(script);}())
+
+//School (idk why it doesn't work right now)
+javascript:(function () {var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/orig_xray/webxray.js';script.className='webxray';script.setAttribute('data-lang','en-US');script.setAttribute('data-baseuri','https://x-ray-goggles.mouse.org');document.body.appendChild(script);}())
