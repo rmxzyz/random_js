92967197183cead5d892c4ea6ef7a73fc1456721
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:04:00 -0700

    Create schoolloop-editor.js

---
+++ other-school-cheats/schoolloop-editor.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var code = document.createElement("script"); code.setAttribute("src","https://cdn.jsdelivr.net/gh/nathansche/schoolloop-editor@master/schoolloopEditor-1.0.js"); document.body.appendChild(code);
