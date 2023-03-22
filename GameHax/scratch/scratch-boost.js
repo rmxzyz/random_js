a22771132fa790db6a46ff948d337259db26cfeb
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:27:00 -0700

    Create scratch-boost.js

---
+++ GameHax/scratch/scratch-boost.js
@@ -0,0 +1,1 @@
+javascript:(function () {var s = document.createElement('script');s.setAttribute('src', 'https://rawgit.com/Iwotastic/Scratch-Boost/master/src/installer.js');document.body.appendChild(s);})();
