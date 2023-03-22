b53e14b033badf11fe337b5b6ce3bbc458c38f59
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 20:53:46 -0700

    Create dark_mode.js

---
+++ GameHax/stmath/dark_mode.js
@@ -0,0 +1,3 @@
+javascript:stcrap:var stylec = document.createElement('style'); stylec.innerText = ':root { \n    --text-blue: #084462; \n    --toolbar-height: 60px; \n    --transition-time: 0.5s; \n   --transition-time-short: 0.3s; \n    --transition-time-long: 0.75s; \n  --sky-blue: #180707; \n    --sky-blue-light: #c27223; \n}'; document.head.appendChild(stylec)
+
+//Finally, night time! only in home though, idk why it doesn't work in normal.
