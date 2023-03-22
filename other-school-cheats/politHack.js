d859c20e197a429404989ff82a86d185d07fb1e9
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:01:44 -0700

    Create politHack.js

---
+++ other-school-cheats/politHack.js
@@ -0,0 +1,3 @@
+javascript:!function(){let t=document.createElement("script");t.type="text/javascript",t.src="https://cdn.jsdelivr.net/gh/lunakoly/PolitHack/hack.js",document.body.appendChild(t),t.onload=t=>{fetch("https://raw.githubusercontent.com/lunakoly/PolitHack/master/polit.json").then(t=>t.json()).then(t=>PolitHack.labelAll(t)).catch(t=>console.error(t))}}();
+
+//credits to lunakoly
