0772e376e60b65866425eebe91e010a217f85c1c
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:09:21 -0700

    Create eruda.js

---
+++ other-school-cheats/eruda.js
@@ -0,0 +1,1 @@
+javascript:(function(){(function(){var r=_=>eruda.initDesktop();if(!window.eruda){var s=document.createElement("script");s.src="https://cdn.jsdelivr.net/gh/BinBashBanana/erudesktop@master/dist/eruda.js";s.onload=r;s.onerror=_=>alert("Failed to load script; The current site\'s policy settings might be blocking this.");document.body.appendChild(s)}else r()})();})();
