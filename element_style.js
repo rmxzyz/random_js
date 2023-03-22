8a69245984b231992f260e3d1091043e2322e342
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 07:03:06 -0700

    Create element_style.js

---
+++ element_style.js
@@ -0,0 +1,1 @@
+javascript:function loadScript(scriptURL) { var scriptElem = document.createElement('SCRIPT'); scriptElem.setAttribute('language', 'JavaScript'); scriptElem.setAttribute('src', scriptURL); document.body.appendChild(scriptElem);}loadScript('https://westciv.com/xray/thexray.js');
