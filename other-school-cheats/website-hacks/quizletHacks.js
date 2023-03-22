193cac25baaf317003a8f89fafb01b0b0a713662
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:17:02 -0700

    Create quizletHacks.js

---
+++ other-school-cheats/website-hacks/quizletHacks.js
@@ -0,0 +1,1 @@
+javascript:quizlHack:(function(){(function(){let x=document.createElement("script");x.src="https://cdn.jsdelivr.net/gh/SnowLord7/quizlet@master/index.js";x.onload=function(){x.remove();};document.body.appendChild(x);})()})()
