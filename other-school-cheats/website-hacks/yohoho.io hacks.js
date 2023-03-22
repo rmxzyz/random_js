8eabde2acebc9c01f008a8793dd109a1460cf6e8
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:18:51 -0700

    Create yohoho.io hacks.js

---
+++ other-school-cheats/website-hacks/yohoho.io hacks.js
@@ -0,0 +1,1 @@
+javascript:(function () {var a = document.createElement('script');a.setAttribute('async', '');a.src = 'https://cdn.jsdelivr.net/gh/bribes/yohoho-hacks-logged-in/setcoins-hack.min.js';document.body.appendChild(a);}())
