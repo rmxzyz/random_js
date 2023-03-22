2333ad7aefe270aae0a7906fced017d82fcb2581
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 07:17:13 -0700

    Create xray_selector.js

---
+++ other/xray_selector.js
@@ -0,0 +1,2 @@
+javascript:xray:function loadScript(scriptURL) { var scriptElem = document.createElement('script'); scriptElem.setAttribute('language', 'JavaScript'); scriptElem.setAttribute('src', scriptURL); document.body.appendChild(scriptElem);}loadScript('https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/xray/thexray.js');
+
