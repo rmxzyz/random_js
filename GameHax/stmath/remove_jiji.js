ba6f0ab53d86499aa90607132d4b85a94a4903b0
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-21 20:18:25 -0700

    Create remove_jiji.js

---
+++ GameHax/stmath/remove_jiji.js
@@ -0,0 +1,3 @@
+javascript:stcrap:document.querySelector('tuxedo-app').children[0].children[3].children[0].children[49].children[0].children[1].children[8].remove()
+
+//i've only tried this on the home page (the clouds and the jet thing)
