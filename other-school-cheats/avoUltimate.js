31a11bc65be0af183b9e5911ab01735c203b46d7
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:05:19 -0700

    Create avoUltimate.js

---
+++ other-school-cheats/avoUltimate.js
@@ -0,0 +1,1 @@
+javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/Browncha023/avo-ultimate@latest/script.min.js';document.body.appendChild(a);}())
