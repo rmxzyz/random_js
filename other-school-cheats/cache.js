85a6aa324ca66ea2b88b398e2b1f338744f8428c
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 12:18:22 -0700

    Create cache.js

---
+++ other-school-cheats/cache.js
@@ -0,0 +1,1 @@
+javascript:(()=>{var unblockedUrl = prompt("enter the url to unblock.");if (unblockedUrl !== null) {location = "http://webcache.googleusercontent.com/search?q=cache:" + escape(unblockedUrl);}})();
