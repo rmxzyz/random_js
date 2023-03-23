618196591ddb26e79c21c199d9a38e1e6039d692
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 19:35:32 -0700

    Create Trap.js

---
+++ fsj/scratch-addons/addon-api/content-scripts/Trap.js
@@ -0,0 +1,115 @@
+import Listenable from "../common/Listenable.js";
+
+/**
+ * Manages object trapping.
+ * @extends Listenable
+ */
+export default class Trap extends Listenable {
+  constructor(tab) {
+    super();
+    this._react_internal_key = undefined;
+    this._isWWW = () => tab.clientVersion === "scratch-www";
+    this._getEditorMode = () => this._isWWW() && tab.editorMode;
+    this._waitForElement = tab.waitForElement.bind(tab);
+    this._cache = Object.create(null);
+  }
+
+  /**
+   * scratch-vm instance.
+   * @throws when on non-project page.
+   * @type {object}
+   */
+  get vm() {
+    if (!this._getEditorMode()) throw new Error("Cannot access vm on non-project page");
+    return __scratchAddonsTraps._onceMap.vm;
+  }
+
+  /**
+   * @private
+   */
+  get REACT_INTERNAL_PREFIX() {
+    return "__reactInternalInstance$";
+  }
+
+  /**
+   * Gets Blockly instance actually used by Scratch.
+   * This is different from window.Blockly.
+   * @async
+   * @throws when on non-project page.
+   * @returns {Promise<object>}
+   */
+  async getBlockly() {
+    if (this._cache.Blockly) return this._cache.Blockly;
+    const editorMode = this._getEditorMode();
+    if (!editorMode || editorMode === "embed")
+      throw new Error(`Cannot access Blockly on ${editorMode} page (${location.pathname})`);
+    const BLOCKS_CLASS = '[class^="gui_blocks-wrapper"]';
+    let elem = document.querySelector(BLOCKS_CLASS);
+    if (!elem) {
+      elem = await this._waitForElement(BLOCKS_CLASS, {
+        reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
+      });
+    }
+    if (!this._react_internal_key) {
+      this._react_internal_key = Object.keys(elem).find((key) => key.startsWith(this.REACT_INTERNAL_PREFIX));
+    }
+    const internal = elem[this._react_internal_key];
+    let childable = internal;
+    /* eslint-disable no-empty */
+    while (((childable = childable.child), !childable || !childable.stateNode || !childable.stateNode.ScratchBlocks)) {}
+    /* eslint-enable no-empty */
+    return (this._cache.Blockly = childable.stateNode.ScratchBlocks);
+  }
+
+  /**
+   * Gets react internal key.
+   * @param {HTMLElement} elem - the reference
+   * @returns {string} the key
+   */
+  getInternalKey(elem) {
+    if (!this._react_internal_key) {
+      this._react_internal_key = Object.keys(elem).find((key) => key.startsWith(this.REACT_INTERNAL_PREFIX));
+    }
+    return this._react_internal_key;
+  }
+
+  /**
+   * Gets @scratch/paper instance.
+   * @async
+   * @throws when on non-project page or if paper couldn't be found.
+   * @returns {Promise<object>}
+   */
+  async getPaper() {
+    if (this._cache.paper) return this._cache.paper;
+    const editorMode = this._getEditorMode();
+    if (!editorMode || editorMode === "embed") throw new Error("Cannot access paper on this page");
+    // We can access paper through .tool on tools, for example:
+    // https://github.com/LLK/scratch-paint/blob/develop/src/containers/bit-brush-mode.jsx#L60-L62
+    // It happens that paper's Tool objects contain a reference to the entirety of paper's scope.
+    const modeSelector = await this._waitForElement("[class*='paint-editor_mode-selector']", {
+      reduxCondition: (state) => state.scratchGui.editorTab.activeTabIndex === 1 && !state.scratchGui.mode.isPlayerOnly,
+    });
+    const internalState = modeSelector[this.getInternalKey(modeSelector)].child;
+    // .tool or .blob.tool only exists on the selected tool
+    let toolState = internalState;
+    let tool;
+    while (toolState) {
+      const toolInstance = toolState.child.stateNode;
+      if (toolInstance.tool) {
+        tool = toolInstance.tool;
+        break;
+      }
+      if (toolInstance.blob && toolInstance.blob.tool) {
+        tool = toolInstance.blob.tool;
+        break;
+      }
+      toolState = toolState.sibling;
+    }
+    if (tool) {
+      const paperScope = tool._scope;
+      this._cache.paper = paperScope;
+      return paperScope;
+    }
+    throw new Error("cannot find paper :(");
+  }
+}
