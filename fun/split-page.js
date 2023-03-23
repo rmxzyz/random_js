b14fb92883fdbdc5dec8f40cff446668ba566831
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-23 15:42:49 -0700

    Create split-page.js

---
+++ fun/split-page.js
@@ -0,0 +1,5 @@
+//horizontally 
+document.write("<HTML><HEAD></HEAD><FRAMESET ROWS='50%,*'><FRAME SRC="+location.href+"><FRAME SRC="+location.href+"></FRAMESET></HTML>"),document.close();
+
+//vertically
+document.write("<HTML><HEAD></HEAD><FRAMESET COLS='50%,*'><FRAME SRC="+location.href+"><FRAME SRC="+location.href+"></FRAMESET></HTML>");
