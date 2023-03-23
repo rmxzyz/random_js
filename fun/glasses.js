17b521fdd3911e4c2a53ec6d49f288e93e79f2ac
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 19:54:35 -0700

    Create glasses.js

---
+++ fun/glasses.js
@@ -0,0 +1,2 @@
+javascript:(e=>{var t,l="*{filter: blur(.7px)}",d="dark-theme-snippet",r=e.getElementById(d);null!=r?r.parentNode.removeChild(r):((t=e.createElement("style")).type="text/css",t.id=d,t.styleSheet?t.styleSheet.cssText=l:t.appendChild(e.createTextNode(l)),(e.head||e.querySelector("head")).appendChild(t))})(document);
+
