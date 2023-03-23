a642b514b7a86e61c3b16f8d547acf2c74fe1bed
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-23 16:00:14 -0700

    Create text-color.esm.js

---
+++ fsj/scratch-addons/custom-set9/lib/text-color.esm.js
@@ -0,0 +1,29 @@
+import * as _textColor from "./text-color.js";
+const {
+    parseHex,
+    convertToHex,
+    convertFromHsv,
+    convertToHsv,
+    brightness,
+    textColor,
+    multiply,
+    brighten,
+    alphaBlend,
+    removeAlpha,
+    makeHsv,
+    recolorFilter,
+} = globalThis.__scratchAddonsTextColor;
+export {
+    parseHex,
+    convertToHex,
+    convertFromHsv,
+    convertToHsv,
+    brightness,
+    textColor,
+    multiply,
+    brighten,
+    alphaBlend,
+    removeAlpha,
+    makeHsv,
+    recolorFilter,
+};
