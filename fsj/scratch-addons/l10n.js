94f49065153ef60c4c0a92a151f67b1a03ade9ef
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 19:21:59 -0700

    Create l10n.js

---
+++ fsj/scratch-addons/l10n.js
@@ -0,0 +1,60 @@
+//import { MessageFormatter, pluralTypeHandler } from "../../thirdparty/cs/icu-message-formatter.es.min.js";
+
+// This library is shared between background and userscript.
+// Subclasses are responsible for implementing methods to load translations.
+
+export default class LocalizationProvider extends EventTarget {
+  constructor() {
+    super();
+    this.messages = {};
+    this._reconfigure();
+  }
+
+  _reconfigure() {
+    const locale = this.locale;
+    this._date = new Intl.DateTimeFormat(locale);
+    this._datetime = new Intl.DateTimeFormat(locale, {
+      timeStyle: "short",
+      dateStyle: "short",
+    });
+    this.formatter = new MessageFormatter(locale, {
+      plural: pluralTypeHandler,
+    });
+  }
+
+  _get(key, placeholders, messageHandler, fallback) {
+    messageHandler = messageHandler || ((m) => m);
+    if (Object.prototype.hasOwnProperty.call(this.messages, key)) {
+      const rawMessage = this.messages[key];
+      // English source file may use Structured JSON, non-English files use keyvalue JSON
+      const message = messageHandler(rawMessage.string || rawMessage);
+      return this.formatter.format(message, placeholders);
+    }
+    if (!fallback) (globalThis.scratchAddons?.console || console).warn("Key missing:", key);
+    return fallback || key;
+  }
+
+  get(key, placeholders = {}, fallback = "") {
+    return this._get(key, placeholders, null, fallback);
+  }
+
+  escaped(key, placeholders = {}, fallback = "") {
+    return this._get(key, placeholders, (message) => escapeHTML(message), fallback);
+  }
+
+  get locale() {
+    return this.messages._locale || "en";
+  }
+
+  get localeName() {
+    return this.messages._locale_name || "English";
+  }
+
+  date(dateObj) {
+    return this._date.format(dateObj);
+  }
+
+  datetime(dateObj) {
+    return this._datetime.format(dateObj);
+  }
+}
