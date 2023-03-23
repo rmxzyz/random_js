14d0fe05f9fb9563d063a0cb4422bc3c842ac017
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 20:47:41 -0700

    Create youtube-tests.js

---
+++ fun/youtube-tests.js
@@ -0,0 +1,14 @@
+var vid = document.querySelector('video')
+if(!vid) {alert("Go to a source with a video.")}
+if(vid) {
+  function pause() {vid.pause()}
+  function play() {vid.play()}
+  var length = document.querySelector("video").duration;   
+  function setPlaybackRate(rate) {vid.playbackRate = rate}
+  function setMuted(bool) {vid.muted = bool}
+  function setVolume(vol) {vid.volume = vol}
+  
+
+  
+  
+}
