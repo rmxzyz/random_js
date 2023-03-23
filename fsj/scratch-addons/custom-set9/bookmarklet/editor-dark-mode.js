c6dd63b991512ddfd9c931df69902b22b27700fe
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-23 16:05:28 -0700

    Create editor-dark-mode.js

---
+++ fsj/scratch-addons/custom-set9/bookmarklet/editor-dark-mode.js
@@ -0,0 +1,3 @@
+const script=document.createElement("script");script.setAttribute("src","https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/scratch-addons/custom-set9/editor-dark-mode.js"),script.setAttribute("async",""),script.setAttribute("type","module"),script.onload=function t(){console.log("Script Loaded")},script.onerror=function t(){alert("Could not load script.")},document.head.appendChild(script);
+
+//damaged for now.
