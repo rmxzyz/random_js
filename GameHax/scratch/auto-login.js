20c41e890cc3cbd76ef0b5dbf4d9a531d445a13c
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-22 18:34:37 -0700

    Create auto-login.js

---
+++ GameHax/scratch/auto-login.js
@@ -0,0 +1,3 @@
+javascript:scratch:function signIn(){document.body.children[2].children[0].children[0].children[0].children[0].children[7].children[1].children[0].children[0].children[4].children[0].children[0].click()}document.body.children[2].children[0].children[0].children[0].children[0].children[7].children[0].children[0].click(),setTimeout(signIn,50);
+
+//run it whenever your signed out, and it will click the buttons.
