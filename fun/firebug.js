b9ab35d3eb4a9c5557d5b2e19931575e1dd5453d
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-24 14:44:28 -0700

    Create firebug.js

---
+++ fun/firebug.js
@@ -0,0 +1,2 @@
+javascript:var firebug=document.createElement('script');firebug.setAttribute('src','https://luphoria.com/fbl/fbl/firebug-lite-debug.js');document.body.appendChild(firebug);(function(){if(window.firebug.version){firebug.init();}else{setTimeout(arguments.callee);}})();void(firebug);
+
