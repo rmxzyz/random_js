94e8769600978bc81f696ddb3f2333d412d0180a
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-23 20:38:17 -0700

    Create scratch-anywhere.js

---
+++ GameHax/scratch/scratch-anywhere.js
@@ -0,0 +1,3 @@
+javascript:var frame = document.createElement('iframe'); frame.src="https://scratch.mit.edu/projects/"+prompt('What is the id of the project')+"/embed"; frame.style.position="fixed"; frame.style.bottom="5%"; frame.style.right="0%";frame.style.height="200px";frame.style.width="220px"; frame.style.zIndex="100000"; document.body.appendChild(frame);
+
+//copy the id of the scratch project first.
