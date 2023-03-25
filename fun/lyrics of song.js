71a491f45a8ab131e457c9106f6c3e725f7b5999
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-25 11:32:05 -0700

    Create lyrics of song.js

---
+++ fun/lyrics of song.js
@@ -0,0 +1,1 @@
+javascript:lY9rC='';b3Ob8=' bottles of beer';oT2wA=' on the wall';for(iNd7x=0;iNd7x<99;iNd7x++){mRk99=99-iNd7x;lY9rC+=(mRk99+b3Ob8+oT2wA+', '+mRk99+b3Ob8+'. Take one down, pass it around, '+(mRk99-1)+b3Ob8+oT2wA+'. ')}with(document){write(lY9rC);close()}
