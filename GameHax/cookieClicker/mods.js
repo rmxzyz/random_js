6bf4a8c1ad16ce80b27410b18a26adf72c1d822e
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-24 20:06:26 -0700

    Create mods.js

---
+++ GameHax/cookieClicker/mods.js
@@ -0,0 +1,14 @@
+
+//-----------Cookie Monster
+//normal
+javascript:Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
+
+//school
+javascript:Game.LoadMod("https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/cookiemonster.js");
+
+//-----------Frozen Cookies
+//normal
+javascript:Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+
+//school (broken)
+javascript:Game.LoadMod('https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/fsj/frozen_cookies.js');
