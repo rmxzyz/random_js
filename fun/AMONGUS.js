e0bdc381f41776ccff9ccb16dbc201e3acff5cdd
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-24 19:47:05 -0700

    Create AMONGUS.js

---
+++ fun/AMONGUS.js
@@ -0,0 +1,1 @@
+javascript:(function(){Array.prototype.slice.call(document.querySelectorAll('img')).map(function(el){el.src = 'https://i.pinimg.com/originals/e8/b3/7a/e8b37aa3bee66e219a10398244734017.gif';});}())
