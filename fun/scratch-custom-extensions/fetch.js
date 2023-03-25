b83528427a3586e6501c8588b46f8137df9b9061
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-03-25 13:03:04 -0700

    Create fetch.js

---
+++ fun/scratch-custom-extensions/fetch.js
@@ -0,0 +1,94 @@
+var Scratch;
+(function(Scratch) {
+  'use strict';
+   function findReactComponent(element) {
+    let fiber = element[Object.keys(element).find(key => key.startsWith("__reactInternalInstance$"))];
+    if (fiber == null) return null;
+
+    const go = fiber => {
+        let parent = fiber.return;
+        while (typeof parent.type == "string") {
+            parent = parent.return;
+        }
+        return parent;
+    };
+    fiber = go(fiber);
+    while(fiber.stateNode == null) {
+        fiber = go(fiber);
+    }
+    return fiber.stateNode;
+}
+  window.vm = findReactComponent(document.getElementsByClassName("stage-header_stage-size-row_14N65")[0]).props.vm;
+
+  // This is for compatibility with plugin loaders that don't implement window.Scratch.
+  // This is a one-time exception. Similar code like this WILL NOT be accepted in new extensions without
+  // significant justification.
+  if (!Scratch) {
+    Scratch = {
+      //@ts-expect-error
+      TargetType: {
+        SPRITE: '_sprite_',
+        STAGE: '_stage_',
+      },
+      // @ts-expect-error
+      BlockType: {
+        COMMAND: 'command',
+        REPORTER: 'reporter',
+        BOOLEAN: 'Boolean',
+        HAT: 'hat'
+      },
+      // @ts-expect-error
+      ArgumentType: {
+        STRING: 'string',
+        NUMBER: 'number',
+        COLOR: 'color',
+        ANGLE: 'angle',
+        BOOLEAN: 'Boolean',
+        MATRIX: 'matrix',
+        NOTE: 'note'
+      },
+      // @ts-expect-error
+      vm: window.vm,
+      extensions: {
+        unsandboxed: true,
+        register: (object) => {
+          // @ts-expect-error
+          const serviceName = vm.extensionManager._registerInternalExtension(object);
+          // @ts-expect-error
+          vm.extensionManager._loadedExtensions.set(object.getInfo().id, serviceName);
+        }
+      }
+    };
+
+  class Fetch {
+    getInfo () {
+      return {
+        id: 'fetch',
+        name: 'Fetch',
+        blocks: [
+          {
+            opcode: 'get',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'GET [URL]',
+            arguments: {
+              URL: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/hello.txt'
+              }
+            }
+          }
+        ]
+      };
+    }
+
+    get (args) {
+      return fetch(args.URL) //Scratch.fetch
+        .then(r => r.text())
+        .catch(() => '');
+    }
+  }
+
+ var extensionInstance = new Fetch(window.vm.extensionManager.runtime)
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
+  }})(window.Scratch);
