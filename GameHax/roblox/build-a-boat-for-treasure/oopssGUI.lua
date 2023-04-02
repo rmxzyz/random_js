62ed23eb7cfab86a4f4d5602e986414da72cbf19
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-04-02 11:54:13 -0700

    Create oopssGUI.lua

---
+++ GameHax/roblox/build-a-boat-for-treasure/oopssGUI.lua
@@ -0,0 +1,10 @@
+
+--Normal
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssGui/main/MainOopssGui.lua", true))()
+
+--School (you probably shouldn't because school computers will crash because of their bad memory lol)
+
+
+--i tested this with krnl. It should work with any other executer.
+--Credits to https://robloxscripter.com/build-a-boat-for-treasure-oopssgui/ .
