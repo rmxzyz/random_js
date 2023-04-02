8fc5807bc5fbc7c22883566306e78c4c30e54578
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-04-02 12:14:46 -0700

    Update MainOopssGui.lua

--- GameHax/roblox/build-a-boat-for-treasure/files/MainOopssGui.lua
+++ GameHax/roblox/build-a-boat-for-treasure/files/MainOopssGui.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssHub/main/Loader.lua"))()
+loadstring(game:HttpGet("https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/GameHax/roblox/build-a-boat-for-treasure/files/MainOopssGui.lua"))()
+-- original: https://raw.githubusercontent.com/OopssSorry/OopssHub/main/Loader.lua
