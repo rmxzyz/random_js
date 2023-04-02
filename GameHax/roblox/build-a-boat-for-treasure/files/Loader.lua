daf8fcab1e5a77b38ef9bf5beefe569a527909b7
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-04-02 12:13:03 -0700

    Create Loader.lua

---
+++ GameHax/roblox/build-a-boat-for-treasure/files/Loader.lua
@@ -0,0 +1,36 @@
+function OopssHubLoader() _G.c4f6632517f847c3892c30b824045738 = { 
+    
+    
+    [537413528] = "537413528", 
+    [7180042682] = "7180042682",
+    [4639625707] = "4639625707"
+    
+    
+  } end 
+OopssHubLoader()
+local def033c227214e549fc4d3d67b7776f4 = nil
+f0cfabc9ecd4454ca6791e1508d068fa = "0000000000"
+c2d0e35c16cb47f4a7ef17abcb005430 = "HttpService"
+c4f6632517f847c3892c30b824045738 = _G.c4f6632517f847c3892c30b824045738
+fe2e9b3b330b4d4794e3343f7437079e = game
+f6bd4bec131b4320b587f0d1c691581a = fe2e9b3b330b4d4794e3343f7437079e.PlaceId
+b6726cac8b124d00b1303959ff99e55a = "https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/"
+lue = ".lua"
+if c4f6632517f847c3892c30b824045738[f6bd4bec131b4320b587f0d1c691581a] ~= nil then
+    loadstring(
+        fe2e9b3b330b4d4794e3343f7437079e:HttpGet(
+      "https://cdn.jsdelivr.net/gh/rmxzyz/random_js@main/GameHax/roblox/build-a-boat-for-treasure/files/BABFT.lua"
+           -- b6726cac8b124d00b1303959ff99e55a .. --https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/
+              --  c4f6632517f847c3892c30b824045738[f6bd4bec131b4320b587f0d1c691581a] .. lue --537413528 .. .lua
+        )
+    )()
+    def033c227214e549fc4d3d67b7776f4 =
+        fe2e9b3b330b4d4794e3343f7437079e:GetService(c2d0e35c16cb47f4a7ef17abcb005430):GenerateGUID()
+end
+if def033c227214e549fc4d3d67b7776f4 == nil then
+    loadstring(
+        fe2e9b3b330b4d4794e3343f7437079e:HttpGet(
+            b6726cac8b124d00b1303959ff99e55a .. f0cfabc9ecd4454ca6791e1508d068fa .. cdb5c35bc16c4f908e4eb5db2249f937 -- 
+        )
+    )()
+end
