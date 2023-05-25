308caf3bf086910bcaea11915d294e3c3a4433c3
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-05-25 16:30:30 -0700

    Create auto_awnser.js

---
+++ GameHax/blooket/royale/auto_awnser.js
@@ -0,0 +1,1 @@
+javascript:(async()=>{setInterval(()=>{var e=Object.values(document.querySelector("body div[id] > div > div"))[1].children[0]._owner["stateNode"];e?.onAnswer?.(!0,e.props.client.question.correctAnswers[0])},50)})();
