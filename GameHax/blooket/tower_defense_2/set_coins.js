43f3feaf422ed8f2feeb58994538cfc5e91f51dc
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-05-25 16:37:22 -0700

    Create set_coins.js

---
+++ GameHax/blooket/tower_defense_2/set_coins.js
@@ -0,0 +1,1 @@
+javascript:(async()=>{var e=document.createElement("iframe");document.body.append(e),window.prompt=e.contentWindow.prompt.bind(window),e.remove(),Object.values(document.querySelector("body div[id] > div > div"))[1].children[0]._owner.stateNode.setState({coins:Number(parseInt(prompt("How many tokens would you like?%22)))})})();
