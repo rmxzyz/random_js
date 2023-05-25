0e6a3bbb56f2bd6c1c04d46323bf0d0ea2ce36f2
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-05-25 16:38:52 -0700

    Create removeducks.js

---
+++ GameHax/blooket/tower_defence/removeducks.js
@@ -0,0 +1,1 @@
+javascript:(async()=>{let{ducks:e,tiles:d}=Object.values(document.querySelector("body div[id] > div > div"))[1].children[0]._owner["stateNode"];e.forEach(e=>{d[e.y][e.x]=0}),e.length=0})();
