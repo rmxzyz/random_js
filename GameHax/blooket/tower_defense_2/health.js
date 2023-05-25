a8a9f4c2d076e49486705a79ea530e8f9d58d80d
randomgames3 <128182562+rmxzyz@users.noreply.github.com>
2023-05-25 16:37:37 -0700

    Create health.js

---
+++ GameHax/blooket/tower_defense_2/health.js
@@ -0,0 +1,1 @@
+javascript:(async()=>{var e=document.createElement("iframe");document.body.append(e),window.prompt=e.contentWindow.prompt.bind(window),e.remove(),Object.values(document.querySelector("body div[id] > div > div"))[1].children[0]._owner.stateNode.setState({health:Number(parseInt(prompt("How much health do you want?%22)))})})();
